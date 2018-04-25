1ac7df6f6ae7880d204b49c2a05ee1f352a22d23
Maarten Pants <maarten@copypants.com>
2018-04-25 15:02:14 -0700

    converting to promises

--- src/lib/Mutex.js
+++ src/lib/Mutex.js
@@ -1,8 +1,8 @@
-const debug = require('debug')('redis-semaphore:mutex:instance')
+const debug = require('debug')('redis-semaphore:mutex:instance');
 
 import { isFunction } from "underscore";
 import LostLockError from "./errors/LostLockError";
-import { refresh, acquire as _acquire, release as _release } from "./mutex";
+import mutex from "./mutex";
 
 const REFRESH_INTERVAL_COEF = 0.8;
 
@@ -60,41 +60,44 @@ class RedisMutex {
   _stopRefresh() {
     clearInterval(this._refreshInterval);
   }
-  async _refresh() {
+  _refresh() {
     debug(`refresh mutex (key: ${this._key}, identifier: ${this._identifier})`);
-    const refreshed = await refresh(
+    return mutex.refresh(
       this._client,
       this._key,
       this._identifier,
       this._lockTimeout
-    );
-    if (!refreshed) {
-      throw new LostLockError(`Lost mutex for key ${this._key}`);
-    }
+    ).tap(refreshed => {
+      if (!refreshed) {
+        throw new LostLockError(`Lost mutex for key ${this._key}`);
+      }
+    });
   }
 
-  async acquire() {
+  acquire() {
     debug(`acquire mutex (key: ${this._key})`);
-    this._identifier = await _acquire(
+    return mutex.acquire(
       this._client,
       this._key,
       this._lockTimeout,
       this._acquireTimeout,
       this._retryInterval
-    );
-    this._startRefresh();
-    return this._identifier;
+    ).then(identifier => {
+      this._identifier = identifier;
+      this._startRefresh();
+      return this._identifier;
+    });
   }
 
-  async release() {
+  release() {
     debug(`release mutex (key: ${this._key}, identifier: ${this._identifier})`);
-    const released = await _release(
+    return mutex.release(
       this._client,
       this._key,
       this._identifier
-    );
-    this._stopRefresh();
-    return released;
+    ).tap(() => {
+      this._stopRefresh();
+    })
   }
 }
 
--- src/lib/mutex/index.js
+++ src/lib/mutex/index.js
@@ -1,48 +1,49 @@
-const TimeoutError = require('../errors/TimeoutError')
-const acquireMutex = require('./acquire')
-const refreshMutex = require('./refresh')
-const releaseMutex = require('./release')
-const uuid4 = require('uuid/v4')
-require('../utils/promisifyRedis')
+const TimeoutError = require('../errors/TimeoutError');
+const acquireMutex = require('./acquire');
+const refreshMutex = require('./refresh');
+const releaseMutex = require('./release');
+const uuid4 = require('uuid/v4');
+require('../utils/promisifyRedis');
 
 function getKey(key) {
-  return `mutex:${key}`
+  return `mutex:${key}`;
 }
 
-async function acquire(
+function acquire(
   client,
   key,
   lockTimeout,
   acquireTimeout,
   retryInterval
 ) {
-  const identifier = uuid4()
-  const finalKey = getKey(key)
-  const result = await acquireMutex(
+  const identifier = uuid4();
+  const finalKey = getKey(key);
+  return acquireMutex(
     client,
     finalKey,
     identifier,
     lockTimeout,
     acquireTimeout,
     retryInterval
-  )
-  if (result) {
-    return identifier
-  } else {
-    throw new TimeoutError(`Acquire mutex ${key} timeout`)
-  }
+  ).then(result => {
+    if (result) {
+      return identifier;
+    } else {
+      throw new TimeoutError(`Acquire mutex ${key} timeout`);
+    }
+  });
 }
 
-async function refresh(client, key, identifier, lockTimeout) {
-  const finalKey = getKey(key)
-  return await refreshMutex(client, finalKey, identifier, lockTimeout)
+function refresh(client, key, identifier, lockTimeout) {
+  const finalKey = getKey(key);
+  return refreshMutex(client, finalKey, identifier, lockTimeout);
 }
 
-async function release(client, key, identifier) {
-  const finalKey = getKey(key)
-  return await releaseMutex(client, finalKey, identifier)
+function release(client, key, identifier) {
+  const finalKey = getKey(key);
+  return releaseMutex(client, finalKey, identifier);
 }
 
-exports.acquire = acquire
-exports.refresh = refresh
-exports.release = release
+exports.acquire = acquire;
+exports.refresh = refresh;
+exports.release = release;
